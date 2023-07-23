7b6d1bd80cff78819b7afc28b91887f6064eb6e7
Monique Silva <moniquesntsilva@gmail.com>
2023-07-22 23:36:00 -0300

    feat: configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsla(0, 0%, 8%)',
+        secondary: 'hsla(259, 100%, 65%)',
+        'light-red': 'hsla(0, 100%, 67%)',
+        'smokey-grey': 'hsla(0, 1%, 44%)',
+        'light-grey': 'hsl(0, 0%, 86%)'
+      },
+      backgroundColor: {
+        white: 'hsla(0, 0%, 100%)',
+        'off-white': 'hsla(0, 0%, 94%)'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
