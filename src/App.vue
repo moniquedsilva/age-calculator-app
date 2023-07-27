d3adbf25c3ff98b3b63847f32034a821dcab1491
Monique Silva <moniquesntsilva@gmail.com>
2023-07-26 21:27:30 -0300

    feat: calculator

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
-import AppInput from './components/global/AppInput.vue'
-import AppButton from './components/global/AppButton.vue'
+import AppForm from './components/AppForm.vue'
 import { ref } from 'vue'
 
 const day = ref('--')
 const month = ref('--')
 const year = ref('--')
-const error = ref({})
-const checkError = ref(false)
+const error = ref('')
 const userDays = ref('')
 const userMonths = ref('')
 const userYears = ref('')
 
 const handleButtonClick = (value) => {
-  error.value = value
-  checkError.value = error.value?.error?.inputError ? true : false
-  if (!checkError.value) {
+  day.value = value.day
+  month.value = value.month
+  year.value = value.year
+  error.value = value.error
+  if (!error.value) {
     calculateAge()
   }
 }
@@ -48,45 +48,34 @@ const calculateAge = () => {
 
 <template>
   <main
-    class="flex h-screen w-screen items-center justify-center bg-off-white p-4 font-poppins"
+    class="flex min-h-screen w-full items-center justify-center bg-off-white p-4 font-poppins md:p-0"
   >
     <section
       class="relative flex h-[500px] w-full max-w-[840px] flex-col justify-center rounded-[22px_22px_100px_22px] bg-white p-6 shadow-2xl shadow-light-grey md:h-[664px] md:p-14"
     >
-      <header class="flex items-center gap-4 md:gap-8">
-        <AppInput v-model="day" label="Day" :error="error" />
-        <AppInput v-model="month" label="Month" :error="error" />
-        <AppInput v-model="year" label="Year" :error="error" />
-      </header>
-      <div class="flex h-32 w-full items-center justify-center md:h-24">
-        <div role="presentation" class="h-[1px] w-full bg-light-grey"></div>
-        <AppButton
-          type="submit"
-          class="absolute md:right-14"
-          :day="day"
-          :month="month"
-          :year="year"
-          @handle-click="handleButtonClick"
-        >
-          <img src="./assets/icon-arrow.svg" alt="arrow" class="w-7" />
-        </AppButton>
-      </div>
+      <AppForm @handle-click="handleButtonClick" />
       <div
         class="space-y-2 text-[56px] font-extrabold italic leading-none text-primary md:text-8xl"
       >
         <p>
-          <span v-if="checkError" class="text-secondary">--</span>
-          <span v-else class="text-secondary">{{ userYears || '--' }}</span>
-          years
+          <span v-if="error" class="tracking-widest text-secondary">--</span>
+          <span v-else class="tracking-widest text-secondary">{{
+            userYears || '--'
+          }}</span
+          >years
         </p>
         <p>
-          <span v-if="checkError" class="text-secondary">--</span>
-          <span v-else class="text-secondary">{{ userMonths || '--' }}</span
+          <span v-if="error" class="tracking-widest text-secondary">--</span>
+          <span v-else class="tracking-widest text-secondary">{{
+            userMonths || '--'
+          }}</span
           >months
         </p>
         <p>
-          <span v-if="checkError" class="text-secondary">--</span>
-          <span v-else class="text-secondary">{{ userDays || '--' }}</span
+          <span v-if="error" class="tracking-widest text-secondary">--</span>
+          <span v-else class="tracking-widest text-secondary">{{
+            userDays || '--'
+          }}</span
           >days
         </p>
       </div>
---
+++ src/components/AppForm.vue
@@ -0,0 +1,237 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const day = ref('')
+const month = ref('')
+const year = ref('')
+const error = ref({
+  day: {
+    empty: false,
+    invalid: false
+  },
+  month: {
+    empty: false,
+    invalid: false
+  },
+  year: {
+    empty: false,
+    invalid: false
+  },
+  inputError: false
+})
+
+const emit = defineEmits(['handleClick'])
+
+const labelClasses = computed(() => {
+  return ['flex', 'flex-col', 'text-sm', 'font-bold', 'uppercase', 'h-32']
+})
+
+const inputClasses = computed(() => {
+  return [
+    'mt-2',
+    'h-[54px]',
+    'w-[90px]',
+    'rounded-md',
+    'border',
+    'border-light-grey',
+    'p-4',
+    'text-xl',
+    'text-primary',
+    '[appearance:textfield]',
+    'focus:border-transparent',
+    'focus:outline-none',
+    'focus:ring-2',
+    'focus:ring-secondary',
+    'md:h-[70px]',
+    'md:w-[158px]',
+    'md:px-6',
+    'md:py-2',
+    'md:text-[32px]',
+    '[&::-webkit-inner-spin-button]:appearance-none',
+    '[&::-webkit-outer-spin-button]:appearance-none'
+  ]
+})
+
+const handleClick = () => {
+  if (day.value === '' || month.value === '' || year.value === '') {
+    validateEmpty()
+    error.value.inputError = true
+    return
+  }
+
+  validateDate()
+
+  emit('handleClick', {
+    day: day.value,
+    month: month.value,
+    year: year.value,
+    error: error.value.inputError
+  })
+}
+
+const validateEmpty = () => {
+  day.value === ''
+    ? (error.value.day.empty = true)
+    : (error.value.day.empty = false)
+
+  month.value === ''
+    ? (error.value.month.empty = true)
+    : (error.value.month.empty = false)
+
+  year.value === ''
+    ? (error.value.year.empty = true)
+    : (error.value.year.empty = false)
+}
+
+const validateDate = () => {
+  const date = new Date(`${month.value}/${day.value}/${year.value}`)
+  const currentYear = new Date().getFullYear()
+
+  console.log(date.getDate())
+  console.log(date)
+
+  if (isNaN(date.getDate())) {
+    error.value.day.invalid = true
+    error.value.inputError = true
+  } else {
+    error.value.day.invalid = false
+  }
+
+  if (isNaN(date.getMonth() + 1)) {
+    error.value.month.invalid = true
+    error.value.inputError = true
+  } else {
+    error.value.month.invalid = false
+  }
+
+  if (isNaN(date.getFullYear())) {
+    error.value.year.invalid = true
+    error.value.inputError = true
+  } else {
+    error.value.year.invalid = false
+  }
+
+  if (year.value > currentYear) {
+    error.value.year.invalid = true
+    error.value.inputError = true
+  } else {
+    error.value.year.invalid = false
+  }
+}
+</script>
+
+<template>
+  <form>
+    <div class="flex items-center gap-4 md:gap-8">
+      <label
+        for="day"
+        :class="[
+          labelClasses,
+          error.day.empty || error.day.invalid
+            ? '!text-red-500'
+            : 'text-smokey-grey'
+        ]"
+      >
+        <span class="tracking-widest">Day</span>
+        <input
+          v-model="day"
+          type="number"
+          :class="[
+            inputClasses,
+            error.day.empty || error.day.invalid
+              ? 'border-red-500'
+              : 'border-light-grey'
+          ]"
+          name="day"
+          placeholder="DD"
+        />
+        <span
+          v-if="error.day.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >This field is required</span
+        >
+        <span
+          v-if="error.day.invalid && !error.day.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >Must be a valid day</span
+        >
+      </label>
+      <label
+        for="month"
+        :class="[
+          labelClasses,
+          error.month.empty || error.month.invalid
+            ? '!text-red-500'
+            : 'text-smokey-grey'
+        ]"
+      >
+        <span class="tracking-widest">Month</span>
+        <input
+          v-model="month"
+          type="number"
+          :class="[
+            inputClasses,
+            error.month.empty || error.month.invalid
+              ? 'border-red-500'
+              : 'border-light-grey'
+          ]"
+          name="month"
+          placeholder="MM"
+        />
+        <span
+          v-if="error.month.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >This field is required</span
+        >
+        <span
+          v-if="error.month.invalid && !error.month.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >Must be a valid month</span
+        >
+      </label>
+      <label
+        for="year"
+        :class="[
+          labelClasses,
+          error.year.empty || error.year.invalid
+            ? '!text-red-500'
+            : 'text-smokey-grey'
+        ]"
+      >
+        <span class="tracking-widest">Year</span>
+        <input
+          v-model="year"
+          type="number"
+          :class="[
+            inputClasses,
+            error.year.empty || error.year.invalid
+              ? 'border-red-500'
+              : 'border-light-grey'
+          ]"
+          name="year"
+          placeholder="YYYY"
+        />
+        <span
+          v-if="error.year.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >This field is required</span
+        >
+        <span
+          v-if="error.year.invalid && !error.year.empty"
+          class="mt-2 text-xs font-normal normal-case italic text-red-500"
+          >Must be in the past</span
+        >
+      </label>
+    </div>
+    <div class="flex h-24 w-full items-center justify-center md:h-16">
+      <div role="presentation" class="h-[1px] w-full bg-light-grey"></div>
+      <button
+        type="submit"
+        class="durantion-200 absolute flex h-16 w-[66px] items-center justify-center rounded-full border-transparent bg-secondary transition-colors hover:bg-primary focus:outline-none focus:ring-transparent md:right-14"
+        @click.prevent="handleClick"
+      >
+        <img src="../assets/icon-arrow.svg" alt="arrow" class="w-7" />
+      </button>
+    </div>
+  </form>
+</template>
